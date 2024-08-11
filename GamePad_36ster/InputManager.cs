6eaf092533b570531623ae712e9aa92cdce8d21e
Mrsuss60 <135135776+Mrsuss60@users.noreply.github.com>
2024-08-11 17:56:52 +0100

    Update InputManager.cs

--- GamePad_36ster/InputManager.cs
+++ GamePad_36ster/InputManager.cs
@@ -1,6 +1,5 @@
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework;
-using System;
 
 namespace GamepadTester
 {
@@ -8,12 +7,6 @@ namespace GamepadTester
     {
         public GamePadState PreviousState { get; private set; }
         public GamePadState CurrentState { get; private set; }
-
-        public bool IsStartButtonDown { get; private set; }
-        public bool WasStartButtonPressed { get; private set; }
-        public bool WasStartButtonReleased { get; private set; }
-        public TimeSpan StartButtonHoldDuration { get; private set; }
-        private DateTime? startButtonPressTime;
 
         public InputManager()
         {
@@ -23,30 +16,9 @@ namespace GamepadTester
 
         public void Update(GameTime gameTime)
         {
+            // Update the previous and current state
             PreviousState = CurrentState;
             CurrentState = GamePad.GetState(PlayerIndex.One);
-
-            bool isStartButtonDownNow = CurrentState.IsButtonDown(Buttons.Start);
-            bool wasStartButtonDownPreviously = PreviousState.IsButtonDown(Buttons.Start);
-
-            WasStartButtonPressed = isStartButtonDownNow && !wasStartButtonDownPreviously;
-            WasStartButtonReleased = !isStartButtonDownNow && wasStartButtonDownPreviously;
-            IsStartButtonDown = isStartButtonDownNow;
-
-            if (WasStartButtonPressed)
-            {
-                startButtonPressTime = DateTime.Now;
-                StartButtonHoldDuration = TimeSpan.Zero;
-            }
-            else if (WasStartButtonReleased)
-            {
-                startButtonPressTime = null;
-                StartButtonHoldDuration = TimeSpan.Zero;
-            }
-            else if (IsStartButtonDown && startButtonPressTime.HasValue)
-            {
-                StartButtonHoldDuration = DateTime.Now - startButtonPressTime.Value;
-            }
         }
     }
 }
