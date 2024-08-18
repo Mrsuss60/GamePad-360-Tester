8252c0b5bbfa761ad862a14a5340f6b7f9950066
Mrsuss60 <135135776+Mrsuss60@users.noreply.github.com>
2024-08-18 15:30:45 +0100

    modified some stuff

--- GamePad_36ster/GamePad_36ster/Backcolors.cs
+++ GamePad_36ster/GamePad_36ster/Backcolors.cs
@@ -12,7 +12,8 @@ public class Backcolors
     private int currentColorIndex = 0;
     private int currentComponentIndex = 0;
     private SpriteFont font;
-    private const float AdjustmentSpeed = 120f; 
+    private const float AdjustmentSpeed = 60f; // Dont go below 60
+
     public Backcolors(SpriteFont font)
     {
         this.font = font;
@@ -21,6 +22,10 @@ public class Backcolors
     public void ToggleActive()
     {
         isActive = !isActive;
+        if (!isActive)
+        {
+            SaveLoad.SaveColors(this);
+        }
     }
 
     public bool IsActive
@@ -42,9 +47,11 @@ public class Backcolors
         if (currentState.DPad.Left == ButtonState.Pressed && previousState.DPad.Left == ButtonState.Released)
             currentComponentIndex = (currentComponentIndex + 2) % 3;
         if (currentState.Buttons.A == ButtonState.Pressed && previousState.Buttons.A == ButtonState.Released)
-            currentColorIndex = (currentColorIndex + 1) % 3;
+            currentColorIndex = (currentColorIndex + 1) % 3; 
+        if (currentState.Buttons.Y == ButtonState.Pressed && previousState.Buttons.Y == ButtonState.Released)
+            currentColorIndex = (currentColorIndex + 2) % 3; 
         if (currentState.Buttons.B == ButtonState.Pressed && previousState.Buttons.B == ButtonState.Released)
-            isActive = false;
+            ToggleActive();
     }
 
     private void AdjustCurrentComponent(int direction, float deltaTime)
@@ -87,22 +94,30 @@ public class Backcolors
             string text = string.Format("{0}: Red:{1} Green:{2} Blue:{3}", colorNames[i], color.R, color.G, color.B);
             Vector2 size = font.MeasureString(text);
             Vector2 position = centerScreen + new Vector2(-size.X / 2, -lineHeight * 2 + i * lineHeight);
-            Color textColor = i == currentColorIndex ? Color.Yellow : Color.Black;
+            Color textColor = Color.Black;
 
             if (i == currentColorIndex)
             {
-                spriteBatch.DrawString(font, ">", position - new Vector2(20, 0), Color.Yellow);
+                spriteBatch.DrawString(font, ">", position - new Vector2(20, 0), Color.Green);
+                textColor = Color.Green;
             }
 
-            spriteBatch.DrawString(font, text, position, textColor);
+            spriteBatch.DrawString(font, colorNames[i] + ":", position, textColor);
 
-            if (i == currentColorIndex)
+            for (int j = 0; j < 3; j++)
             {
-                Vector2 componentPos = position + new Vector2(size.X / 2 + 10, 0);
+                string componentText = j == 0 ? string.Format(" Red:{0}", color.R) :
+                                       (j == 1 ? string.Format(" Green:{0}", color.G) :
+                                       string.Format(" Blue:{0}", color.B));
+                float additionalSpacing = j == 2 ? 20 : 0; 
+                Vector2 componentPosition = position + new Vector2(font.MeasureString(colorNames[i] + ":").X, 0) + new Vector2(j * 100 + additionalSpacing, 0);
+
+                Color componentColor = (i == currentColorIndex && j == currentComponentIndex) ? Color.Yellow : Color.Black;
+                spriteBatch.DrawString(font, componentText, componentPosition, componentColor);
             }
         }
 
-        string instructions = "D-Pad: change color values | A: Switch up or down | B: Exit";
+        string instructions = "D-Pad: change color values | A: Next | Y: Previous | B: Exit/Save";
         Vector2 instructionsSize = font.MeasureString(instructions);
         Vector2 instructionsPosition = new Vector2(screenWidth / 2 - instructionsSize.X / 2, screenHeight - 30);
         spriteBatch.DrawString(font, instructions, instructionsPosition, Color.Black);
@@ -113,5 +128,12 @@ public class Backcolors
         topLeft = this.topLeft;
         topRight = this.topRight;
         bottom = this.bottom;
+    }
+
+    public void SetColors(Color topLeft, Color topRight, Color bottom)
+    {
+        this.topLeft = topLeft;
+        this.topRight = topRight;
+        this.bottom = bottom;
     }
 }
